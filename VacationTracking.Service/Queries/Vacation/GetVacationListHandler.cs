e827e9a8164762cc8eb69f671d1129a8393318c6
brkzkn <burakozkan123@gmail.com>
2020-06-08 21:23:01 +0300

    - deleted old repository pattern

--- VacationTracking.Service/Queries/Vacation/GetVacationListHandler.cs
+++ VacationTracking.Service/Queries/Vacation/GetVacationListHandler.cs
@@ -6,42 +6,45 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using VacationTracking.Data.IRepositories;
+using VacationTracking.Data.Repository;
 using VacationTracking.Domain.Dtos;
 using VacationTracking.Domain.Queries.Vacation;
+using VacationDb = VacationTracking.Domain.Models.Vacation;
 
 namespace VacationTracking.Service.Queries.Vacation
 {
     public class GetVacationListHandler : IRequestHandler<GetVacationListQuery, IList<VacationDto>>
     {
-        private readonly IVacationRepository _vacationRepository;
+        private readonly IRepository<VacationDb> _repository;
         private readonly ILogger _logger;
         private readonly IMapper _mapper;
 
-        public GetVacationListHandler(IMapper mapper, ILogger<GetVacationListHandler> logger)
+        public GetVacationListHandler(IMapper mapper, IRepository<VacationDb> repository, ILogger<GetVacationListHandler> logger)
         {
-            _vacationRepository = null;// vacationRepository ?? throw new ArgumentNullException(nameof(vacationRepository));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
         public async Task<IList<VacationDto>> Handle(GetVacationListQuery request, CancellationToken cancellationToken)
         {
+            throw new NotImplementedException();
+
             // TODO: Check user permission. 
             // User should has owner or admin permission
-            var vacations = await _vacationRepository.GetListAsync(request.CompanyId);
+            //var vacations = await _vacationRepository.GetListAsync(request.CompanyId);
 
-            if (vacations != null)
-            {
-                _logger.LogInformation($"Got a request get vacation by CompanyId: {request.CompanyId}");
-                List<VacationDto> vacationDtos = new List<VacationDto>();
-                foreach (var vacation in vacations)
-                {
-                    vacationDtos.Add(_mapper.Map<VacationDto>(vacation));
-                }
-                return vacationDtos;
-            }
+            //if (vacations != null)
+            //{
+            //    _logger.LogInformation($"Got a request get vacation by CompanyId: {request.CompanyId}");
+            //    List<VacationDto> vacationDtos = new List<VacationDto>();
+            //    foreach (var vacation in vacations)
+            //    {
+            //        vacationDtos.Add(_mapper.Map<VacationDto>(vacation));
+            //    }
+            //    return vacationDtos;
+            //}
 
-            return null;
+            //return null;
         }
     }
 }
